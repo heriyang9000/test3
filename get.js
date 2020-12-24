1dd325199e54c2fcd97d098dd393508463c5c51c
heriyang9000 <40530246+heriyang9000@users.noreply.github.com>
2020-12-25 05:23:30 +0700

    Update get.js

--- get.js
+++ get.js
@@ -1,7 +1,17 @@
 function get() {
-$.get("https://query2.finance.yahoo.com/v8/finance/chart/A?formatted=true&crumb=&lang=en-US&region=US&period1=1118568991&period2=1608868503&interval=1d&events=div%7Csplit&corsDomain=finance.yahoo.com").done(function (data) {
-    console.log(data);
-});
+// $.get("https://query2.finance.yahoo.com/v8/finance/chart/A?formatted=true&crumb=&lang=en-US&region=US&period1=1118568991&period2=1608868503&interval=1d&events=div%7Csplit&corsDomain=finance.yahoo.com").done(function (data) {
+//     console.log(data);
+// });
+$.ajax({            
+    url: "https://query2.finance.yahoo.com/v8/finance/chart/A?formatted=true&crumb=&lang=en-US&region=US&period1=1118568991&period2=1608868503&interval=1d&events=div%7Csplit&corsDomain=finance.yahoo.com",
+    contentType: "application/jsonp",
+    dataType: 'json',
+    success: function(result){                     
+      var yahoo_data = result;
+      console.log(yahoo_data);         
+    
+    }
+  }) 
 }
 
 get();
